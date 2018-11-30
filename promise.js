f34ae8309d7517d224e7bd5cebf181578d096367
songxiujin <18049468+songxiujin@users.noreply.github.com>
2018-11-30 13:23:00 +0800

    Create promise.js
    promise的基本常识

---
+++ promise.js
@@ -0,0 +1,47 @@
+for (var i = 0; i < 5; i++) {
+    setTimeout(function() {
+        console.log(new Date, i);
+    }, 1000);
+}
+
+console.log(new Date, i);
+//
+for (var i = 0; i < 5; i++) {
+    (function(j) {  // j = i
+        setTimeout(function() {
+            console.log(new Date, j);
+        }, 1000);
+    })(i);
+}
+
+console.log(new Date, i);
+//
+var output = function (i) {
+    setTimeout(function() {
+        console.log(new Date, i);
+    }, 1000);
+};
+
+for (var i = 0; i < 5; i++) {
+    output(i);  // 这里传过去的 i 值被复制了
+}
+
+console.log(new Date, i);
+//
+const tasks = [];
+for (var i = 0; i < 5; i++) {   // 这里 i 的声明不能改成 let，如果要改该怎么做？
+    ((j) => {
+        tasks.push(new Promise((resolve) => {
+            setTimeout(() => {
+                console.log(new Date, j);
+                resolve();  // 这里一定要 resolve，否则代码不会按预期 work
+            }, 1000 * j);   // 定时器的超时时间逐步增加
+        }));
+    })(i);
+}
+
+Promise.all(tasks).then(() => {
+    setTimeout(() => {
+        console.log(new Date, i);
+    }, 1000);   // 注意这里只需要把超时设置为 1 秒
+});
